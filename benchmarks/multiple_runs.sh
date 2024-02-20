3b4c11ff71078bb17d65733c6ee917b50a5b164d
Yazeed Almalaq <124050604+yalmalaq@users.noreply.github.com>
2024-02-20 13:38:58 +0000

    Add files via upload

---
+++ benchmarks/multiple_runs.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#This script will run android apps ten times and after each times will cloes all the opened apps and pages, the will store the outputs result in txt file
+
+    ##### to weak up and unlcock the screen ###########
+ unlock_screen() {
+    ./adb shell input keyevent KEYCODE_WAKEUP
+    sleep 2
+    ./adb shell input swipe 500 1200 500 500
+    sleep 2
+}
+
+
+    ##### close all apps func ###
+close_all_apps() {
+    ./adb shell input keyevent KEYCODE_APP_SWITCH
+    sleep 2
+    ./adb shell input swipe 100 500 1000 500
+    sleep 2
+    ./adb shell input swipe 100 500 1000 500
+    sleep 2
+    ./adb shell input swipe 100 500 1000 500
+    sleep 2
+    ./adb shell input touchscreen tap 200 1200
+    sleep 2
+}
+
+## ./adb shell settings put system screen_brightness 100
+#Lock Screen:
+#./adb shell input keyevent KEYCODE_POWER
+
+
+for i in {1..10}; do
+    ./run.sh >> BatterySaver_backgroundmusic_all_t10.txt
+    sleep 2
+    
+     unlock_screen
+     sleep 1
+    
+
+    close_all_apps
+    sleep 1
+done
