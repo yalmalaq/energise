19b79965bed9dbc0aee59323309ca7836ba0c42a
Yazeed Almalaq <124050604+yalmalaq@users.noreply.github.com>
2024-03-08 17:44:22 +0000

    Add files via upload

--- benchmarks/apps.sh
+++ benchmarks/apps.sh
@@ -33,34 +33,34 @@ keep_screen_on() {
     ./adb shell svc power stayon false
 }
 
-
 for i in {1..2}; do
     ./adb shell am start -n com.zhiliaoapp.musically/com.ss.android.ugc.aweme.splash.SplashActivity
         sleep 2
-    ./adb shell am start -n com.twitter.android/com.twitter.app.main.MainActivit
+    ./adb shell am start -n com.twitter.android/com.twitter.android.StartActivity
     sleep 2
-    ./adb shell dumpsys gfxinfo com.zhiliaoapp.musically %s | grep -i jank -C 5
-    ./adb shell dumpsys gfxinfo com.twitter.android %s | grep -i jank -C 5
-
-    ./runsocialmedia.sh
-    ./adb shell dumpsys gfxinfo com.zhiliaoapp.musically %s | grep -i jank -C 5
-    ./adb shell dumpsys gfxinfo com.twitter.android %s | grep -i jank -C 5
+    echo "Timestamp: $(date)" >> qos_tiktok.txt; ./adb shell dumpsys gfxinfo com.zhiliaoapp.musically %s | grep -i jank -C 5 >> qos_tiktok.txt  2>&1
+        sleep 2
+       echo "Timestamp: $(date)" >> qos_twitter.txt;  ./adb shell dumpsys gfxinfo com.twitter.android %s | grep -i jank -C 5 >>  qos_twitter.txt  2>&1
+    sleep 2
+    ./runsocialmedia.sh >> qos_twitter.txt
+        sleep 2
+     echo "Timestamp: $(date)" >> qos_tiktok.txt; ./adb shell dumpsys gfxinfo com.zhiliaoapp.musically %s | grep -i jank -C 5 >> qos_tiktok.txt  2>&1
+        sleep 2
+       echo "Timestamp: $(date)" >> qos_twitter.txt;  ./adb shell dumpsys gfxinfo com.twitter.android %s | grep -i jank -C 5 >>  qos_twitter.txt  2>&1
     sleep 2
     unlock_screen
     sleep 2
     close_all_apps
     sleep 2
- 
-
 done
 
 for i in {1..2}; do
    ./adb shell am start -n com.JindoBlu.OfflineGames/com.unity3d.player.UnityPlayerActivity
         sleep 2
-    ./adb shell dumpsys gfxinfo com.JindoBlu.OfflineGames %s | grep -i jank -C 5
-   
-    ./rungame.sh
-    ./adb shell dumpsys gfxinfo com.JindoBlu.OfflineGames %s | grep -i jank -C 5
+      echo "Timestamp: $(date)" >> qos_game.txt; ./adb shell dumpsys gfxinfo com.JindoBlu.OfflineGames %s | grep -i jank -C 5 >> qos_game.txt 2>&1
+
+    ./rungame.sh >> qos_game.txt 2>&1
+      echo "Timestamp: $(date)" >> qos_game.txt; ./adb shell dumpsys gfxinfo com.JindoBlu.OfflineGames %s | grep -i jank -C 5 >> qos_game.txt 2>&1
     sleep 2
     unlock_screen
     sleep 2
@@ -72,10 +72,10 @@ done
 for i in {1..2}; do
    ./adb shell am start -n com.microsoft.teams/com.microsoft.skype.teams.Launcher
         sleep 2
-    ./adb shell dumpsys gfxinfo com.microsoft.teams %s | grep -i jank -C 5
-   
-    ./runvideocall.sh
-    ./adb shell dumpsys gfxinfo com.microsoft.teams %s | grep -i jank -C 5
+      echo "Timestamp: $(date)" >> qos_video.txt; ./adb shell dumpsys gfxinfo com.microsoft.teams %s | grep -i jank -C 5 >> qos_video.txt 2>&1
+
+    ./runvideocall.sh >> qos_video.txt 2>&1
+         echo "Timestamp: $(date)" >> qos_video.txt; ./adb shell dumpsys gfxinfo com.microsoft.teams %s | grep -i jank -C 5 >> qos_video.txt 2>&1
     sleep 2
     unlock_screen
     sleep 2
@@ -87,10 +87,10 @@ for i in {1..2}; do
   ./adb shell am start -n com.soundcloud.android/.launcher.LauncherActivity
   
         sleep 2
-    ./adb shell dumpsys gfxinfo com.soundcloud.android %s | grep -i jank -C 5
-   
-    ./runbackgroundmusic.sh
-    ./adb shell dumpsys gfxinfo com.soundcloud.android %s | grep -i jank -C 5
+     echo "Timestamp: $(date)" >> qos_music.txt; ./adb shell dumpsys gfxinfo com.soundcloud.android %s | grep -i jank -C 5 >> qos_music.txt 2>&1
+
+    ./runbackgroundmusic.sh >> qos_music.txt 2>&1
+     echo "Timestamp: $(date)" >> qos_music.txt; ./adb shell dumpsys gfxinfo com.soundcloud.android %s | grep -i jank -C 5 >> qos_music.txt 2>&1
     sleep 2
     unlock_screen
     sleep 2
@@ -101,15 +101,16 @@ done
 for i in {1..2}; do
   ./adb shell am start -n org.lineageos.jelly/.MainActivity
         sleep 1
-    ./adb shell dumpsys gfxinfo org.lineageos.jelly %s | grep -i jank -C 5 >> qos_data.txt
-   
-    ./runweb.sh  >> fps_data.txt
+  echo "Timestamp: $(date)" >> qos_web.txt; ./adb shell dumpsys gfxinfo org.lineageos.jelly %s | grep -i jank -C 5 >> qos_web.txt 2>&1
+
+    ./runweb.sh  >> qos_web.txt
     sleep 1
-    ./adb shell dumpsys gfxinfo org.lineageos.jelly %s | grep -i jank -C 5 >> qos_data.txt
+  echo "Timestamp: $(date)" >> qos_web.txt; ./adb shell dumpsys gfxinfo org.lineageos.jelly %s | grep -i jank -C 5 >> qos_web.txt 2>&1
     sleep 2
     unlock_screen
     sleep 2
     close_all_apps
     sleep 2
 done
-
+#
+#
