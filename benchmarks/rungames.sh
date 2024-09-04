3f1b0b412ad533869505be0f10e830896c2214d2
chrisbrown1982 <chrisbrown.guitar@googlemail.com>
2024-09-04 15:14:16 +0100

    scripts added

--- benchmarks/rungames.sh
+++ benchmarks/rungames.sh
@@ -1,16 +1,17 @@
-OUTPUT_FILE=output/output_$(date '+%y-%m-%d_%H-%M').txt
+OUTPUT_FILE=output/output_$(date '+%y-%m-%d_%H-%M')_games.txt
 
 echo "**** run starting $(date)" >> $OUTPUT_FILE
 
 remote_adb shell sh /data/ryan/unlock.sh
 remote_adb shell sh /data/ryan/prevent_sleep.sh
 
-for i in $(seq 5); do
-    sh runone.sh apps/web-task.cpp | tee -a $OUTPUT_FILE
-    sh runone.sh apps/background-music.cpp | tee -a $OUTPUT_FILE
+for i in $(seq 1); do
+    #sh runone.sh apps/web-task.cpp | tee -a $OUTPUT_FILE
+    #sh runone.sh apps/background-music.cpp | tee -a $OUTPUT_FILE
     sh runone.sh apps/games.cpp | tee -a $OUTPUT_FILE
-    sh runone.sh apps/idle.cpp | tee -a $OUTPUT_FILE
-    sh runone.sh apps/social-media.cpp | tee -a $OUTPUT_FILE
+    #sh runone.sh apps/idle.cpp | tee -a $OUTPUT_FILE
+    #sh runone.sh apps/social-media.cpp | tee -a $OUTPUT_FILE
+    
     #sh runone.sh video-call.cpp | tee -a output
     #video playing?
 done
@@ -18,6 +19,6 @@ done
 remote_adb shell sh /data/ryan/allow_sleep.sh
 remote_adb shell sh /data/ryan/clear.sh
 
-notify "done"
+echo "done"
 
 
--- benchmarks/runweb.sh
+++ benchmarks/runweb.sh
@@ -1,4 +1,4 @@
-OUTPUT_FILE=output/output_$(date '+%y-%m-%d_%H-%M').txt
+OUTPUT_FILE=output/output_$(date '+%y-%m-%d_%H-%M')_web_social.txt
 
 echo "**** run starting $(date)" >> $OUTPUT_FILE
 
@@ -7,10 +7,11 @@ remote_adb shell sh /data/ryan/prevent_sleep.sh
 
 for i in $(seq 5); do
     sh runone.sh apps/web-task.cpp | tee -a $OUTPUT_FILE
-    sh runone.sh apps/background-music.cpp | tee -a $OUTPUT_FILE
-    sh runone.sh apps/games.cpp | tee -a $OUTPUT_FILE
-    sh runone.sh apps/idle.cpp | tee -a $OUTPUT_FILE
+    #sh runone.sh apps/background-music.cpp | tee -a $OUTPUT_FILE
+    #sh runone.sh apps/games.cpp | tee -a $OUTPUT_FILE
+    #sh runone.sh apps/idle.cpp | tee -a $OUTPUT_FILE
     sh runone.sh apps/social-media.cpp | tee -a $OUTPUT_FILE
+    
     #sh runone.sh video-call.cpp | tee -a output
     #video playing?
 done
@@ -18,6 +19,6 @@ done
 remote_adb shell sh /data/ryan/allow_sleep.sh
 remote_adb shell sh /data/ryan/clear.sh
 
-notify "done"
+echo "done"
 
 
