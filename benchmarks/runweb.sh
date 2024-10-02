45f9379ed8cca9cefe98afd7c4f4ad925e5ab270
Chris Brown <chrisbrown.guitar@googlemail.com>
2024-10-02 16:41:54 +0100

    base results

--- benchmarks/runweb.sh
+++ benchmarks/runweb.sh
@@ -1,11 +1,11 @@
-OUTPUT_FILE=output/output_$(date '+%y-%m-%d_%H-%M')_web_social.txt
+OUTPUT_FILE=output/base/output_$(date '+%y-%m-%d_%H-%M')_web.txt
 
 echo "**** run starting $(date)" >> $OUTPUT_FILE
 
 remote_adb shell sh /data/ryan/unlock.sh
 remote_adb shell sh /data/ryan/prevent_sleep.sh
 
-for i in $(seq 1); do
+for i in $(seq 5); do
     sh runone.sh apps/web-task.cpp | tee -a $OUTPUT_FILE
     #sh runone.sh apps/background-music.cpp | tee -a $OUTPUT_FILE
     #sh runone.sh apps/games.cpp | tee -a $OUTPUT_FILE
