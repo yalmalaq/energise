a42f96d9ee1ea59cf1a6dd6cc7948d260f112632
chrisbrown1982 <chrisbrown.guitar@gmail.com>
2024-10-03 15:18:30 +0100

    updated scripts

--- benchmarks/runmusic.sh
+++ benchmarks/runmusic.sh
@@ -2,10 +2,11 @@ OUTPUT_FILE=output/base/output_$(date '+%y-%m-%d_%H-%M')_music.txt
 
 echo "**** run starting $(date)" >> $OUTPUT_FILE
 
-remote_adb shell sh /data/ryan/unlock.sh
-remote_adb shell sh /data/ryan/prevent_sleep.sh
-
 for i in $(seq 1); do
+
+    remote_adb shell sh /data/ryan/unlock.sh
+    remote_adb shell sh /data/ryan/prevent_sleep.sh
+
     #sh runone.sh apps/web-task.cpp | tee -a $OUTPUT_FILE
     sh runone.sh apps/background-music.cpp | tee -a $OUTPUT_FILE
     #sh runone.sh apps/games.cpp | tee -a $OUTPUT_FILE
@@ -14,10 +15,14 @@ for i in $(seq 1); do
     
     #sh runone.sh video-call.cpp | tee -a output
     #video playing?
+
+    sleep 5
+
+    remote_adb shell sh /data/ryan/allow_sleep.sh
+    remote_adb shell sh /data/ryan/clear.sh
+
 done
 
-remote_adb shell sh /data/ryan/allow_sleep.sh
-remote_adb shell sh /data/ryan/clear.sh
 
 echo "done"
 
