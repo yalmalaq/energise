c603defb1437256ff3d29832a0f2ed3a4375ba03
chrisbrown1982 <chrisbrown.guitar@gmail.com>
2024-10-03 15:22:08 +0100

    youtube script added

---
+++ benchmarks/runyoutube.sh
@@ -0,0 +1,29 @@
+OUTPUT_FILE=output/base/output_$(date '+%y-%m-%d_%H-%M')_youtube.txt
+
+echo "**** run starting $(date)" >> $OUTPUT_FILE
+
+
+for i in $(seq 5); do
+    remote_adb shell sh /data/ryan/unlock.sh
+    remote_adb shell sh /data/ryan/prevent_sleep.sh
+
+    sh runone.sh apps/youtube.cpp | tee -a $OUTPUT_FILE
+    #sh runone.sh apps/background-music.cpp | tee -a $OUTPUT_FILE
+    #sh runone.sh apps/games.cpp | tee -a $OUTPUT_FILE
+    #sh runone.sh apps/idle.cpp | tee -a $OUTPUT_FILE
+    #sh runone.sh apps/social-media.cpp | tee -a $OUTPUT_FILE
+    
+    #sh runone.sh video-call.cpp | tee -a output
+    #video playing?
+
+    sleep 5
+
+    remote_adb shell sh /data/ryan/allow_sleep.sh
+    remote_adb shell sh /data/ryan/clear.sh
+
+done
+
+
+echo "done"
+
+
