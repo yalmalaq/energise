a57a8f718a73c43308e867dba571b08d6703221e
Chris Brown <chrisbrown.guitar@googlemail.com>
2024-10-18 16:13:40 +0100

    exhaustive tests

--- benchmarks/runweb.sh
+++ benchmarks/runweb.sh
@@ -1,4 +1,4 @@
-OUTPUT_FILE=output/sleepy_run/output_$(date '+%y-%m-%d_%H-%M')_web.txt
+OUTPUT_FILE=output/exhaustive_run/output_$(date '+%y-%m-%d_%H-%M')_web_4s2l.txt
 
 echo "**** run starting $(date)" >> $OUTPUT_FILE
 
