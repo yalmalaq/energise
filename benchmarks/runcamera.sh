7f45c41a10a12a00e002ce052616efda6d5fa088
Chris Brown <chrisbrown.guitar@googlemail.com>
2024-10-25 13:51:50 +0100

    camera results

--- benchmarks/runcamera.sh
+++ benchmarks/runcamera.sh
@@ -1,4 +1,4 @@
-OUTPUT_FILE=output/exhaustive_run/output_$(date '+%y-%m-%d_%H-%M')_camera.txt
+OUTPUT_FILE=output/sleepy_run/output_$(date '+%y-%m-%d_%H-%M')_camera.txt
 
 echo "**** run starting $(date)" >> $OUTPUT_FILE
 
